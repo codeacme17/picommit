1efbc6118c0be65a191d5d7a8987627e9deec1d0
codeacme17 <vir_kings@qq.com>
2023-09-15 22:52:11 +0800

    chore: remove `sharp` and add `image-size`

--- test/process-image.test.ts
+++ test/process-image.test.ts
@@ -1,24 +1,22 @@
-import sharp from 'sharp'
+import path from 'path'
+import imageSize from 'image-size'
 import { readPicommitConfig } from '../src/main'
-import {
-  getImagesFromDocs,
-  processImages,
-} from '../src/process-images'
+import { getImagesFromDocs, processImages } from '../src/process-images'
 
 describe('Process Images functions', () => {
   it('process imaged', async () => {
     const config = readPicommitConfig()
     await processImages(config)
-    const imagePath = 'test_docs\\test.png'
-    const metadata = await sharp(imagePath).metadata()
-    expect(metadata.width).toBe(800)
+
+    const imagePath = path.resolve(__dirname, '../test_docs/test.png')
+    const dimensions = imageSize(imagePath)
+
+    expect(dimensions.width).toBe(800)
   })
 
   it('gets images from a given directory', async () => {
     const config = readPicommitConfig()
     const images = getImagesFromDocs('./test_docs', config)
-    expect(images).toEqual(
-      expect.arrayContaining(['test_docs\\test.png'])
-    )
+    expect(images).toEqual(['test_docs/test.png'])
   })
 })
